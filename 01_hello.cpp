58d45af6df211c9598a8fc5b76fc939cd85fc1b0
Shivam Mishra <84087343+Shiv4mcodes@users.noreply.github.com>
2026-07-10 22:22:30 +0530

    Hello C++

---
+++ 01_hello.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+int main(){
+    std::cout<<"Hello World";
+    return 0;
+}
+
+#include<iostream>
+int main(){
+    std::cout<<"Hellowww lil bitch";
+    return 0;
+}
+
+#include <iostream>
+int main(){
+    std::cout<<"hellol";
+    return 0;
+}
